022058046f9fb543c8f3b39a57c62590efd67bb0
jerrysu5379 <jerrysu5379@gmail.com>
2023-08-06 12:37:11 +0800

    new

---
+++ src/Test01.java
@@ -0,0 +1,20 @@
+/**
+ * test
+ */
+public class Test01 {
+    public static void main(String[] args){
+        int age=18;
+        int $age=19;
+        int _age=20;
+        int age123=21;
+        //int 123age=22; 数字不能做开头
+        //int age#=23; 标识符只能是字母、数字、下划线、$
+        int 年龄=24;//不推荐
+        int _class=25;
+        int monthSalary=1500;
+        int annualSalary=monthSalary*12;
+        System.out.println(annualSalary);
+        double bonus=300.1;
+        System.out.println(bonus);
+    }
+}

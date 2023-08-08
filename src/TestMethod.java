4a754ca745ca0c72ee9e5951954e1c05ff6ea3f4
jerrysu5379 <jerrysu5379@gmail.com>
2023-08-08 21:35:23 +0800

    update

--- src/TestMethod.java
+++ src/TestMethod.java
@@ -11,8 +11,7 @@ public class TestMethod {
         printInfo();
     }
     public static int add(int n1,int n2){
-        int sum=n1+n2;
-        return sum;
+        return n1+n2;
     }
     public static void printInfo(){
         System.out.println("JerrySu5379");

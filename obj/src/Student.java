b17b49762c6b63e3c1fd4ce77e19c13578d207d8
jerrysu5379 <jerrysu5379@gmail.com>
2023-08-09 12:54:08 +0800

    update

---
+++ obj/src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    int id;
+    String name;
+    int score;
+    int age;
+
+    public void study(){
+        System.out.println("正在学习"+name);
+    }
+
+    public static void main(String[] args) {
+        Student stu01
+    }
+}

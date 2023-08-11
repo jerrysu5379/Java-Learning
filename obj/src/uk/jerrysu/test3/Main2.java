eb11d669cceed05f78262784073dadafecec34df
jerrysu5379 <jerrysu5379@gmail.com>
2023-08-11 19:08:53 +0800

    new

---
+++ obj/src/uk/jerrysu/test3/Main2.java
@@ -0,0 +1,12 @@
+package uk.jerrysu.test3;
+
+import static java.lang.Math.*;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Math.random();
+        random();
+        System.out.println(random());
+        System.out.println(PI);
+    }
+}
